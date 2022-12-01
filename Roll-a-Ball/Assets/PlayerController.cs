09818b85d1fed711c89c794341949f3785db3389
jenny_death <gjrot139@learner.dvusd.org>
2022-12-01 08:27:52 -0700

    Roll-a-Ball movement progress

---
+++ Roll-a-Ball/Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.get<Vector2>;
+    }
+
+}
