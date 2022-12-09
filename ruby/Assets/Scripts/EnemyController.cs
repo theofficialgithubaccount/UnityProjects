d42890cb6842f906ec6edb405b3e02ee43ee054f
kid named finger <gjrot139@learner.dvusd.org>
2022-12-08 20:47:02 -0700

    Ruby Progress 3
    Now on HUD

--- ruby/Assets/Scripts/EnemyController.cs
+++ ruby/Assets/Scripts/EnemyController.cs
@@ -8,9 +8,12 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+    bool broken = true;
 
     Animator animator;
 
@@ -24,6 +27,12 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if (!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -35,6 +44,12 @@ public class EnemyController : MonoBehaviour
 
     void FixedUpdate()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if (!broken)
+        {
+            return;
+        }
+
         Vector2 position = rigidbody2D.position;
 
         if (vertical)
@@ -61,5 +76,16 @@ public class EnemyController : MonoBehaviour
         {
             player.ChangeHealth(-1);
         }
+    }
+
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        //optional if you added the fixed animation
+        animator.SetTrigger("Fixed");
+
+        smokeEffect.Stop();
     }
 }
