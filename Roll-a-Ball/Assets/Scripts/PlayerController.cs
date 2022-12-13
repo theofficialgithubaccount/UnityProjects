bd0ebd326be5b3ce4044693ab85c01010d74caf4
jenny_death <gjrot139@learner.dvusd.org>
2022-12-13 09:31:36 -0700

    Roll-a-Ball Tuesday Update
    Completed pickup collision section

--- Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -30,4 +30,14 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+      
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }     
+        
+    }
 }
